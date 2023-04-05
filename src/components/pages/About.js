aea5e6c3fc6327c00b63bcd3ea5d566a19bafff4
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2023-04-05 12:30:17 -0400

    add footer add about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,12 @@ export default function About() {
       <h2 className="text-center">About Lucas</h2>
       <p className="text-center">
         Lucas Bielinski is a recent bootcamp graduate intrested in web
-        development. This portfolio contains some of his many projects.
+        development. This portfolio contains some of his many projects. He has
+        covere a lot of brand-new territory in his recent bootcamp, learning
+        something new every-week of the 24-week experince. He has experince in
+        full-stack development, using HTML, JavaScript, CSS, React, Node.js,
+        Express.js and more. The projects on this page and the page itself is a
+        product of those experinces.
       </p>
     </div>
   );
